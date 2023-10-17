6cdd57cbffec29530ae8afeb530171c9898ea706
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-17 22:45:14 +0200

    first commit

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int i = 0;
+        int resualt = 1;
+        while (i < power) {
+            resualt *= numberToPrint;
+            ++i;
+        }
+        System.out.println(resualt);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int i, j;
 
+        for (i = 1; i <= cathetusLength; i++) {
+            for (int k = 0; k < cathetusLength - i; k++ ) {
+                System.out.print(" ");
+            }
+             for (j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+
+            for (j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
